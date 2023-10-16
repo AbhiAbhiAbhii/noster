4949995a8ebcaeb9c746565e1972175c03ab1dcc
AbhiAbhiAbhii <abhilash@gradical.xyz>
2023-10-16 17:04:00 +0530

    Lenis nav scrollTo update

--- app/Component/NavComponent/NavComponentA.js
+++ app/Component/NavComponent/NavComponentA.js
@@ -2,10 +2,41 @@ import { useEffect } from "react"
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 
+import Lenis from '@studio-freight/lenis'
+
+
 gsap.registerPlugin(ScrollTrigger)
 
 
-export default function NavComponentA()  {
+export default function NavComponentA( )  {
+    
+
+    let lenis;
+    if (typeof window !== "undefined") {
+    lenis = new Lenis({
+        duration: 1.2,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+        direction: 'vertical', // vertical, horizontal
+        gestureDirection: 'vertical', // vertical, horizontal, both
+        smooth: true,
+        mouseMultiplier: 1,
+        smoothTouch: true,
+        touchMultiplier: 2,
+        infinite: false,
+    })
+
+    // lenis.on('scroll', (e) => {
+    //   console.log(e, "Lenis Scrolling")
+
+    // })
+
+    function raf(time) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
+
+    }
 
     let data = [
         {
@@ -18,7 +49,7 @@ export default function NavComponentA()  {
         },
         {
           text:'Execution',
-          link:'/'
+          link:'.b-scene-frame'
         },
       ]
 
@@ -38,7 +69,6 @@ export default function NavComponentA()  {
             },10)
         })
 
-        let elipseContainer = document.querySelectorAll('.elipse-container')
         let elipse = document.querySelectorAll('.elipse')
 
         const observer = new IntersectionObserver((items) => {
@@ -75,8 +105,6 @@ export default function NavComponentA()  {
             })
         }, {threshold: 0.2})
         observerC.observe(document.querySelector('.b-scene-frame'))
-
-        
       })
 
 
@@ -86,11 +114,13 @@ export default function NavComponentA()  {
             {
             data.map((item, i) => {
                 return(
-                <div className="works-nav-item snow" key={i} style={{borderRight: i != 2 ? '1px #000 solid':'', position:'relative'}}>
+                <div onClick={() => {
+                   lenis.scrollTo(`${item.link}`)
+                }} className="works-nav-item snow" key={i} style={{borderRight: i != 2 ? '1px #000 solid':'', position:'relative', cursor:'pointer'}}>
                     <div className="elipse-container" style={{width:'10%', display:'flex', alignItems:'center', justifyContent:'center', overflow:'hidden', position:'relative', left:'-5%'}}>
                         <div className="elipse" style={{height:'0.5em', width:'0.5em', background:'#000', borderRadius:'50%', position:'relative', opacity:'0'}} />
                     </div>
-                    <a className="black-txt" href={item.link} style={{position:'relative', width:'60%', display:'flex', alignItems:'center', justifyContent:'flex-start'}}>
+                    <a className="black-txt"  style={{position:'relative', width:'60%', display:'flex', alignItems:'center', justifyContent:'flex-start'}}>
                     { item.text }
                     </a>
                 </div>
--- app/layout.js
+++ app/layout.js
@@ -102,7 +102,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <ParallaxProvider>
       <Nav />
-      <body className={inter.className}>{children}</body>
+      <body lenis={Lenis} className={inter.className}>{children}</body>
       <Footer />
       </ParallaxProvider>
     </html>
