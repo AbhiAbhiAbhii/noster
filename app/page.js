dfcdc889bc61e02fbe9fd11c949a6e68db064dbd
AbhiAbhiAbhii <abhilash@gradical.xyz>
2024-02-16 16:23:49 +0530

    Testing

--- app/page.js
+++ app/page.js
@@ -3,17 +3,11 @@ import { SliceZone } from "@prismicio/react";
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
 
-export default async function Home() {
+export default async function Page() {
   const client = createClient();
   const page = await client.getSingle("home");
 
-  console.log(page,"adjlashjd")
-
-  return (
-    <>
-    <SliceZone slices={page.data.slices} components={components} />
-    </>
-  )
+  return <SliceZone slices={page.data.slices} components={components} />;
 }
 
 export async function generateMetadata() {
