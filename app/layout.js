a49c9236657a4c4021493e916b0bc8f0d9221574
AbhiAbhiAbhii <abhilash@gradical.xyz>
2024-02-28 13:02:38 +0530

    Lenis update

--- app/layout.js
+++ app/layout.js
@@ -36,33 +36,36 @@ export default function RootLayout({ children }) {
   
   let lenis;
 
-  var speed;
+  // var speed;
 
-  if(window.innerWidth <= 780) {
-    speed = 1
-  } else speed = 1.001
-
+  // if(window.innerWidth <= 780) {
+  //   speed = 1
+  // } else speed = 1.001
 
 
   if (typeof window !== "undefined") {
-    lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, speed - Math.pow(2, -10 * t)), 
-      // https://www.desmos.com/calculator/brs54l4xou
-      direction: 'vertical', // vertical, horizontal
-      gestureDirection: 'vertical', // vertical, horizontal, both
-      smooth: true,
-      mouseMultiplier: 1,
-      smoothTouch: true,
-      touchMultiplier: 2,
-      infinite: false,
-      
-    })
-    function raf(time) {
-      lenis.raf(time)
+    if(window.innerWidth >= 580) {
+      lenis = new Lenis({
+        duration: 1.2,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
+        // https://www.desmos.com/calculator/brs54l4xou
+        direction: 'vertical', // vertical, horizontal
+        gestureDirection: 'vertical', // vertical, horizontal, both
+        smooth: true,
+        mouseMultiplier: 1,
+        smoothTouch: true,
+        touchMultiplier: 2,
+        infinite: false,
+        
+      })
+      function raf(time) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+      }
       requestAnimationFrame(raf)
+    } else  {
+      null
     }
-    requestAnimationFrame(raf)
   }
   }, [])
 
