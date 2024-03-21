5724b9b2fbf0edc275ebc84d35938ab6e11e8e28
AbhiAbhiAbhii <abhilash@gradical.xyz>
2024-03-21 13:23:35 +0530

    Update

--- app/Component/video-modal.js
+++ app/Component/video-modal.js
@@ -4,7 +4,7 @@ import CloseBtn from './close-btn'
 const VideoModal = ({ videoLink, closeModal, modalVidRef }) => {
   return (
     <div className='video-modal-layer-A'>
-        <div onClick={closeModal} className='video-modal-layer-B'>
+        <div className='video-modal-layer-B'>
             <div className='video-modal-video-container'>
                 <video ref={modalVidRef} controls autoPlay muted="true" loop="true" preload="metadata" playsInline="true" loading="lazy">
                     <source src={videoLink} type="video/mp4" />
